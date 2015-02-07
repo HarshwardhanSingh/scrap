1a3c0f5363f2e39c7f7848930688e45d3e97aec3
HarshwardhanSingh <harsh.rathore14@gmail.com>
2015-02-07 16:35:13 +0530

    fixed js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require dropzone
 //= require_tree .
 $(function(){
 	$('.signup-newsletter-btn').click(function(event){
